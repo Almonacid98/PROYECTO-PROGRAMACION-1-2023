cbb7ee8b9371a3359783913cb1ae1ea7046a25a1
D3XTRO12 <mirazopablo@gmail.com>
2023-06-06 10:08:59 -0300

    Modificacion a nivel visual de css, cambio de background y carrito, acorde a la paleta de colores, como asi tambien menus de signup y login

--- mi_primer_proyecto_react/src/components/Home.jsx
+++ mi_primer_proyecto_react/src/components/Home.jsx
@@ -21,27 +21,27 @@ export const Home = () =>{
       </div>
       <div className="Seguinos">
       <h2>¡Seguinos también en nuestras redes sociales!</h2>
-      <table>
+      <table className='social-media'>
         <tr>
-          <td className='col-md-1'>
-          <td className="col-md-1">
-            <a href="https://es-la.facebook.com/">
-              <img src={require(`../images/face.webp`)} width="100" height="100" alt="Facebook" />
-            </a>
-          </td>
-          <td className="col-md-1">
-            <a href="https://www.instagram.com/">
-              <img src={require(`../images/174855.png`)} width="70" height="70" alt="Instagram" />
-            </a>
-          </td>
-          <td className="col-md-1">
-            <a href="https://www.google.com/maps/place/Universidad+de+Mendoza+Sede+San+Rafael/@-34.6135595,-68.327293,17.31z/data=!4m6!3m5!1s0x967907fb83d8ff41:0xc950900df2c99fb6!8m2!3d-34.61368!4d-68.327453!16s%2Fg%2F1tdx0rv1?hl=es&entry=ttu">
-              <img src={require(`../images/google-maps-logo-2-1.png`)} width="70" height="70" alt="googlemaps"/>
-            </a>
-            <h5>
-              ENCONTRANOS!!
-            </h5>
-          </td>
+          <td className='col-md-7'>
+            <td className="col-md-1">
+              <a href="https://es-la.facebook.com/">
+                <img src={require(`../images/face.webp`)} width="100" height="100" alt="Facebook" />
+              </a>
+            </td>
+            <td className="col-md-1">
+              <a href="https://www.instagram.com/">
+                <img src={require(`../images/174855.png`)} width="70" height="70" alt="Instagram" />
+              </a>
+            </td>
+            <td className="col-md-1">
+              <a href="https://www.google.com/maps/place/Universidad+de+Mendoza+Sede+San+Rafael/@-34.6135595,-68.327293,17.31z/data=!4m6!3m5!1s0x967907fb83d8ff41:0xc950900df2c99fb6!8m2!3d-34.61368!4d-68.327453!16s%2Fg%2F1tdx0rv1?hl=es&entry=ttu">
+                <img src={require(`../images/google-maps-logo-2-1.png`)} width="70" height="70" alt="googlemaps"/>
+              </a>
+              <h5>
+                ENCONTRANOS!!
+              </h5>
+            </td>
           </td>
         </tr>
       </table>
