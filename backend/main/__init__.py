91dc841620621f6bd4da9b8c9a58d19c23c43800
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-06-13 20:35:31 -0300

    Actualizacion MI BACKEND / PROFES fusion

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,36 +2,20 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
-import main.resources as resources 
+from config import Config
+
 
 api = Api()
 
-def create_app():
+def create_app(config_object='config.DevelopmentConfig'):
     app = Flask(__name__)
-    load_dotenv()
-    """
-    api.add_resource(resources.BolsonesResource, '/bolsones')
-    api.add_resource(resources.BolsonResource, '/bolson/<id>')
+    app.config.from_object(config_object)
 
-    api.add_resource(resources.BolsonesVentaResource, '/bolsones-venta')
-    api.add_resource(resources.BolsonVentaResource, '/bolson-venta/<id>')
+    from app.api import example_blueprint
+    app.register_blueprint(example_blueprint)
 
-    api.add_resource(resources.BolsonesPendientesResource, '/bolsones-pendientes')
-    api.add_resource(resources.BolsonPendienteResource, '/bolson-pendiente/<id>')
+    @app.errorhandler(404)
+    def not_found(error):
+        return {'STATUS': 'NO ESISTE KPO'}, 404
 
-    api.add_resource(resources.BolsonesPreviosResource, '/bolsones-previos')
-    api.add_resource(resources.BolsonPrevioResource, '/bolson-previo/<id>')
-
-    api.add_resource(resources.ProductosResource, '/productos')
-    api.add_resource(resources.ProductoResource, '/producto/<id>')
-
-    api.add_resource(resources.ComprasResource, '/compras')
-    api.add_resource(resources.CompraResource, '/compra/<id>')
-
-    api.add_resource(resources.ClientesResource, '/clientes')
-    api.add_resource(resources.ClienteResource, '/cliente/<id>')
-
-    api.add_resource(resources.ProveedoresResource, '/proveedores')
-    api.add_resource(resources.ProveedorResource, '/proveedor/<id>')"""
-    api.init_app(app)
     return app
---
+++ backend/main/api/routes/routes.py
@@ -0,0 +1,16 @@
+from flask import jsonify, request
+from app.api import example_blueprint
+
+
+@example_blueprint.route('/ola', methods=['GET'])
+def get_examples():
+    data = {"message": "Recibido kpo", "method": "GET"}
+    return jsonify(data), 200
+
+
+@example_blueprint.route('/', methods=['POST'])
+def create_example():
+    # request_data = request.get_json()
+    data = {"message": "Success", "method": "POST"}
+    return jsonify(data), 201
+ 
