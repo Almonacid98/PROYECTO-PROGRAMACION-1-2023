7cd5c661abc656d1acaa8b9a675c95113d519032
D3XTRO12 <mirazopablo@gmail.com>
2023-06-17 11:40:15 -0300

    aplicacion de axios para login y Signup, aplicacion de formik en signup

--- mi_primer_proyecto_react/src/components/LoginSignUp/Login.jsx
+++ mi_primer_proyecto_react/src/components/LoginSignUp/Login.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
-import {Formik, Form, Field} from 'formik';
+import { Formik, Form, Field } from 'formik';
 import './Login.css';
 import axios from 'axios';
-export const Login = () => {
 
+export const Login = () => {
   const initialValues = {
-    email:'',
-    password:''
-  }
-  const handleLogin = async(values) => { 
-    // console.log('values:', values) 
-    try { 
-      const response = await axios.post("http://localhost:5000/example/ola", values) 
-      console.log(response.data) 
-    } catch (error) { 
-      console.log(error) 
-    } 
-  }
+    email: '',
+    password: ''
+  };
+
+  const handleLogin = async (values) => {
+    try {
+      const baseURL = 'http://127.0.0.1:5000'; //ip default en flask
+      const response = await axios.post(`${baseURL}/example/ola`, values);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    // 
-    <Formik className='formik-section'
+    <Formik
       initialValues={initialValues}
-      onSubmit={handleLogin}>
+      onSubmit={handleLogin}
+    >
       <Form className='form-formik'>
         <h2>Iniciar sesión</h2>
-          <Field
-            type='email'
-            placeholder="alguien@example.com"
-            name='email' />
-          <Field
-            type="password"
-            placeholder="Contraseña"
-            name='password' />
-          <button type='submit' onClick={handleLogin}>Iniciar sesión</button>
+        <Field
+          type='email'
+          placeholder="alguien@example.com"
+          name='email'
+        />
+        <Field
+          type="password"
+          placeholder="Contraseña"
+          name='password'
+        />
+        <button type='submit'>Iniciar sesión</button>
       </Form>
     </Formik>
   );
-}
+};
--- mi_primer_proyecto_react/src/components/LoginSignUp/SignUp.jsx
+++ mi_primer_proyecto_react/src/components/LoginSignUp/SignUp.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
-import './SignUp.css'; 
+import { Formik, Form, Field } from 'formik';
+import './SignUp.css';
+import axios from 'axios';
 
 export const SignUp = () => {
+  const handleSignUp = async (values) => {
+    try {
+      const baseURL = 'http://127.0.0.1:5000'; ///ip default en flask
+      const response = await axios.post(`${baseURL}/example/ola`, values);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const initialValues = {
+    name: '',
+    email: '',
+    phone: '',
+    password: ''
+  };
+
   return (
     <div className="register-section">
       <div className="register-form">
         <h2>Registrarse</h2>
-        <form>
-          <input type="text" placeholder="Nombre" />
-          <input type="email" placeholder="Correo electrónico" />
-          <input type="number" placeholder="Telefono"/>
-          <input type="password" placeholder="Contraseña" />
-          <button type="submit">Registrarse</button>
-        </form>
+        <Formik initialValues={initialValues} onSubmit={handleSignUp}>
+          <Form>
+            <Field
+              type="text"
+              placeholder="Nombre"
+              name="name"
+              className="input-field"
+            />
+            <Field
+              type="email"
+              placeholder="Correo electrónico"
+              name="email"
+              className="input-field"
+            />
+            <Field
+              type="number"
+              placeholder="Teléfono"
+              name="phone"
+              className="input-field"
+            />
+            <Field
+              type="password"
+              placeholder="Contraseña"
+              name="password"
+              className="input-field"
+            />
+            <button type="submit" className="submit-button">
+              Registrarse
+            </button>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
-}
-
+};
